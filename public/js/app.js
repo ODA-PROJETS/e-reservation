56a1d8d53bdeca250bd784b1f68c81124dbe81b2
sminth <emmanuelmalan225@gmail.com>
2020-12-28 00:16:33 +0000

    .

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\personnel\Documents\Programming Universe\1-Projects\projets laragon\e-reservation\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\personnel\Documents\Programming Universe\1-Projects\projets laragon\e-reservation\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\jeton\Desktop\sminth travail\web\ereservation\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\jeton\Desktop\sminth travail\web\ereservation\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
